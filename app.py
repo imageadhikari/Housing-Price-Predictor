8ff71a593ff6f6e5f7077e31f9d36030762ce82e
imageadhikari <imageadhikari@gmail.com>
2022-08-11 22:46:28 +0545

    Started implementing home page with html and css

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+import pickle
+import numpy as np
 
+model = pickle.load(open('predictor.pkl','rb'))
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return render_template("index.html")
 
 if __name__=="__main__":
