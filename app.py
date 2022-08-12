c69b0705f2fc5680617840106d5be505267e232f
imageadhikari <imageadhikari@gmail.com>
2022-08-13 00:06:34 +0545

    Basic CSS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
@@ -8,6 +8,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    # HTML -> .py
+    if request.method == "POST":
+        name = request.form["Land Area"]
+
+    #.py -> HTML    
+    return render_template('predict.html', n = name)
 
 if __name__=="__main__":
     app.run(debug=True)
